57fdaff98efe561a9417d84d4edccad4e990c1fc
afsalmern <afsal@intersmart.in>
2024-04-25 15:42:59 +0530

    fixed issue in counterjs

--- components/layouts/Counter.js
+++ components/layouts/Counter.js
@@ -1,7 +1,7 @@
 var React = require("react");
+var ReactDOM = require("react-dom");
 var Counter = require("react-counter");
 
-React.render(
-  <Counter begin={0} end={1000} time={2000} easing="outCube" />,
-  document.body
+ReactDOM.createRoot(document.body).render(
+  <Counter begin={0} end={1000} time={2000} easing="outCube" />
 );
