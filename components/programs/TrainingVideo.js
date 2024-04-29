67302e7b5f56562972296480d67a5ccc2d4d43b3
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-04-30 01:33:51 +0530

    program details page api integration

--- components/programs/TrainingVideo.js
+++ components/programs/TrainingVideo.js
@@ -2,14 +2,15 @@ import Image from "next/image";
 import { useTranslation } from "react-i18next";
 import styles from "@/styles/Program.module.scss";
 
-const TrainingVideo = ({ Lang }) => {
+const TrainingVideo = ({ Lang, CourseByIdArray }) => {
   const { t } = useTranslation();
   return (
     <div className={styles.trainingVideo_section}>
       <div className={"container"}>
         <div className={styles.imgWrap}>
           <video muted autoPlay loop playsInline preload="metadata" aria-label="video player">
-            <source src="/videos/programVideo.mp4" type="video/mp4" />
+            <source src={`${process.env.customKey}/courseImages/${CourseByIdArray?.videoUrl}`} type="video/mp4" />
+            {/* <source src="/videos/programVideo.mp4" type="video/mp4" /> */}
           </video>
         </div>
       </div>
