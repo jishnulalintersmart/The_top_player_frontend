fb88d9dae284813777cff234e49899b6d76233b4
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-04-29 19:15:04 +0530

    modified program details page

--- components/programs/Football.js
+++ components/programs/Football.js
@@ -5,103 +5,84 @@ import { FaDumbbell } from "react-icons/fa";
 const FootballProgram = ({ styles, Lang }) => {
   const { t } = useTranslation();
   return (
-  <div className={Lang === "ar" ? "progrss_ar" : "progrss_en"}>
+    <div className={Lang === "ar" ? "progrss_ar" : "progrss_en"}>
       <div className="row align-items-center justify-content-between">
-      <div className="col-md-8">
-        <h1 className="title">{t("programs_details.football.title")}</h1>
+        <div className="col-md-8">
+          <h1 className="title">{t("programs_details.football.title")}</h1>
 
-        <p className={styles.program_p_2}>{t("programs_details.contain")}</p>
+          <p className={styles.program_p_2}>{t("programs_details.contain")}</p>
 
-        <div
-          className={`${styles.Program} ${
-            Lang === "ar" ? styles.Ar_program : ""
-          }`}
-        >
-          <ul>
-            <li>
-              <div className={styles.check}>
-                <span className={styles.dumbel}>
-                  <FaDumbbell />
-                </span>
-                <p>
-                        {t("programs.football.line1_1")}
-                        <span className="En_num">
-                          {t("programs.football.line1_2")}
-                        </span>
-                        {t("programs.football.line1_3")}
-                      </p>
-              </div>
-            </li>
-            <li>
-              <div className={styles.check}>
-                <span className={styles.dumbel}>
-                  <FaDumbbell />
-                </span>
-                <p>
-                        <span className="En_num">
-                          {t("programs.football.line2_1")}
-                        </span>
-                        {t("programs.football.line2_2")}
-                        <span className="En_num">
-                          {t("programs.football.line2_3")}
-                        </span>
-                        {t("programs.football.line2_4")}
-                      </p>
-              </div>
-            </li>
-            <li>
-              <div className={styles.check}>
-                <span className={styles.dumbel}>
-                  <FaDumbbell />
-                </span>
-                <p>
-                        {t("programs.football.line3_1")}
-                        <span className="En_num">
-                          {t("programs.football.line3_2")}
-                        </span>
-                        {t("programs.football.line3_3")}
-                      </p>
-              </div>
-            </li>
-            <li>
-              <div className={styles.check}>
-                <span className={styles.dumbel}>
-                  <FaDumbbell />
-                </span>
-                <p>{t("programs_details.football.line4")}</p>
-              </div>
-            </li>
+          <div className={`${styles.Program} ${Lang === "ar" ? styles.Ar_program : ""}`}>
+            <ul>
+              <li>
+                <div className={styles.check}>
+                  <span className={styles.dumbel}>
+                    <FaDumbbell />
+                  </span>
+                  <p>
+                    {t("programs.football.line1_1")}
+                    <span className="En_num">{t("programs.football.line1_2")}</span>
+                    {t("programs.football.line1_3")}
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div className={styles.check}>
+                  <span className={styles.dumbel}>
+                    <FaDumbbell />
+                  </span>
+                  <p>
+                    <span className="En_num">{t("programs.football.line2_1")}</span>
+                    {t("programs.football.line2_2")}
+                    <span className="En_num">{t("programs.football.line2_3")}</span>
+                    {t("programs.football.line2_4")}
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div className={styles.check}>
+                  <span className={styles.dumbel}>
+                    <FaDumbbell />
+                  </span>
+                  <p>
+                    {t("programs.football.line3_1")}
+                    <span className="En_num">{t("programs.football.line3_2")}</span>
+                    {t("programs.football.line3_3")}
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div className={styles.check}>
+                  <span className={styles.dumbel}>
+                    <FaDumbbell />
+                  </span>
+                  <p>{t("programs_details.football.line4")}</p>
+                </div>
+              </li>
 
-            <li>
-              <div className={styles.check}>
-                <span className={styles.dumbel}>
-                  <FaDumbbell />
-                </span>
-                <p>
-                        {t("programs.football.line5_1")}
+              <li>
+                <div className={styles.check}>
+                  <span className={styles.dumbel}>
+                    <FaDumbbell />
+                  </span>
+                  <p>
+                    {t("programs.football.line5_1")}
 
-                        <span className="En_num">
-                          {t("programs.football.line5_2")}
-                        </span>
-                        {t("programs.football.line5_3")}
-                      </p>
-              </div>
-            </li>
-          </ul>
+                    <span className="En_num">{t("programs.football.line5_2")}</span>
+                    {t("programs.football.line5_3")}
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="col-md-4">
-        <div className={styles.card_image}>
-          <Image
-            src={"/images/3.png"}
-            alt="fitness"
-            layout="fill"
-            objectFit="contain"
-          />
+        <div className="col-md-4">
+          <div className={styles.card_image}>
+            <Image src={"/images/3.png"} alt="fitness" layout="fill" objectFit="contain" />
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
