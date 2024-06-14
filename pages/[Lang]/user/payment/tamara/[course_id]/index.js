6554a163c5294e560f1294f0bfe9ce9b6e222689
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2024-06-14 15:49:01 +0530

    added amount too

--- pages/[Lang]/user/payment/tamara/[course_id]/index.js
+++ pages/[Lang]/user/payment/tamara/[course_id]/index.js
@@ -51,7 +51,7 @@ const Payment = ({ course_id, Lang, CourseByIdArray }) => {
     axios
       .post(
         `${process.env.customKey}/create-tamara-payment`,
-        { courseId: course_id, lang: Lang },
+        { courseId: course_id, lang: Lang, amount: CourseByIdArray?.offerAmount },
         {
           headers: {
             "Content-Type": "application/json",
